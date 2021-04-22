9839b2005bd5f04bc5b7fde4b6c44bc0d688b3be
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2021-04-22 15:49:46 +0400

    second

--- src/JavaBase/AsString.java
+++ src/JavaBase/AsString.java
@@ -1,8 +1,6 @@
 package JavaBase;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class AsString {
     public static void main(String[] args) {
@@ -10,22 +8,27 @@ public class AsString {
         Scanner scanner=new Scanner(System.in).useLocale(Locale.forLanguageTag("en-US"));
 
         List <Integer> list = new ArrayList<>();
-        List <Integer> res = new ArrayList<>();
         while (scanner.hasNext()) {
             try {
-                list.add(Integer.valueOf(scanner.next()));
-            } catch (InputMismatchException e) {
-                    scanner.next();
+                String temp = scanner.next();
+                if (temp.equals("exit")) break;
+                list.add(Integer.valueOf(temp));
+            } catch (NumberFormatException ignored) {
             }
         }
+
+        List <Integer> res = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             if (!(i%2==0)) res.add(list.get(i));
         }
         Collections.reverse(res);
 
-        System.out.print(String.valueOf(res).replace("[", "").replace("]", "").replace(",",""));
+        System.out.println(String.valueOf(res).replace("[", "")
+                .replace("]", "")
+                .replace(",",""));
+
 
     }
 
-
 }
+
