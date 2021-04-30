09863f608c7ba8008ab1de555580773e4b80eced
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2021-04-30 11:58:30 +0400

    +версия с рефакторингом после фейла с мерджем

--- src/ToDoProject/Abstractions/ITaskList.java
+++
@@ -1,6 +0,0 @@
-package ToDoProject.Abstractions;
-
-public interface ITaskList {
-    void editTask (String userInput);
-    void newTask();
-}
--- src/ToDoProject/Main.java
+++ src/ToDoProject/Main.java
@@ -3,28 +3,43 @@ package ToDoProject;
 import ToDoProject.Models.Task;
 import ToDoProject.Storages.FileStorage;
 import ToDoProject.UserInterfaces.ConsoleUserInterface;
-
 import java.io.IOException;
 import java.time.temporal.ValueRange;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-        var storage = new FileStorage("D:\\ToDoList.txt");
-        var taskList = new TaskList(storage.getAll());
-        var ui = new ConsoleUserInterface();
-
         try {
+            var storage = new FileStorage("D:\\ToDoList.txt");
+            var ui = new ConsoleUserInterface();
             while (true) {
-                String userInput=getUserInput("Выберете номер пункта, создайте новую задачу (NEW) или EXIT - выйти из программы");
-                if (userInput.equals("NEW")) taskList.newTask();
-                else taskList.editTask(userInput);
+                for (Task task:storage.getAll()) {
+                    ui.show(task.toString());
+                }
+                String userInput;
+                if (storage.getAll().length>0) {
+                    userInput = ui.askInput("Введите новую заметку, BACK - к списку заметок или EXIT - выйти из программы");
+                    if (!userInput.equals("BACK")) storage.add(new Task(storage.getAll().length + 1, userInput, new Date(), "Wait"));
+                }
+                else {
+                    userInput = ui.askInput("Выберете номер пункта, создайте новую задачу (NEW) или EXIT - выйти из программы");
+                    if (userInput.equals("NEW")) {
+                        userInput = ui.askInput("Введите новую заметку, BACK - к списку заметок или EXIT - выйти из программы");
+                        if (!userInput.equals("BACK")) storage.add(new Task(storage.getAll().length + 1, userInput, new Date(), "Wait"));
+                    } else {
+                        int taskIndex = getIndex(userInput, storage.getAll());
+                        if (taskIndex != -1) do {
+                            userInput = ui.askInput("DEL - удалить, DONE - пометить как выполненное, WAIT - возобновить задачу," +
+                                    " BACK - к списку заметок или EXIT - выйти из программы ");
+                            if (userInput.equals("DONE")) storage.setStatus(taskIndex - 1,"Done");
+                            if (userInput.equals("WAIT")) storage.setStatus(taskIndex - 1,"Wait");
+                            if (userInput.equals("DEL")) storage.delete(taskIndex - 1);
+                        } while (!Main.commandInList(userInput, new String[]{"DEL", "DONE", "WAIT", "BACK", "EXIT"}));
+                    }
+                }
             }
         } catch (IOException e) { System.out.println("Файл не найден"); }
     }
-
 
     static boolean commandInList (String userInput, String[] list) {
         return Arrays.asList(list).contains(userInput);
@@ -35,12 +50,13 @@ public class Main {
         System.exit(0);
     }
 
-    static String getUserInput (String message) {
-        var scanner = new Scanner((System.in)).useDelimiter("\n");
-        System.out.println(message);
-        String userInput = scanner.next();
-        if (userInput.equals("EXIT")) exit();
-        return userInput;
+    static int getIndex (String userInput, Task[] tasks) {
+        try {
+            int taskIndex = Integer.parseInt(userInput);
+            if (ValueRange.of(1, tasks.length).isValidIntValue(taskIndex)) return taskIndex;
+            else return -1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
-
 }
