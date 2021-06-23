3f25183a19e01ae88a7ac3b87109b6cb0068582b
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2021-06-23 16:25:24 +0400

    ~todo: рефакторинг логики

--- src/main/java/toDoProject/Main.java
+++ src/main/java/toDoProject/Main.java
@@ -13,18 +13,16 @@ import java.util.*;
 public class Main {
     public static void main (String[] args) {
         Map<String, String> env = System.getenv();
-
         var ui = new ConsoleUserInterface();
 //        var ui = new WindowUserInterface();
 //        var ui = new TelegramBotUserInterface(env.get("BotToken"));
         String userInput = "";
-        while (userInput.length()==0 || Arrays.stream(new String[]{"\\", "|", "/", ":", "?", "\"", "<", ">"}).anyMatch(userInput::contains)) {
-            userInput = ui.askInput("Введите имя пользователя");
+        String[] invalidSymbols = new String[]{" ", "\\", "|", "/", ":", "?", "\"", "<", ">"};
+        while (userInput.length()==0 || Arrays.stream(invalidSymbols).anyMatch(userInput::contains)) {
+            userInput = ui.askInput("Введите имя");
         }
-
-        var storage = new DBStorage(env.get("dbUserName"), env.get("dbUserPass"), userInput);
-//        var storage = new FileStorage("D:\\" + userInput + ".txt");
-
+//        var storage = new DBStorage(env.get("dbUserName"), env.get("dbUserPass"), userInput);
+        var storage = new FileStorage("D:\\" + userInput + ".txt");
         while (true) {
             Task[] arrTask = storage.getAll();
             Map<Integer, Task> map = new LinkedHashMap<>();
@@ -36,34 +34,28 @@ public class Main {
                     countToShow += 1;
                 }
             }
-
-            if (arrTask.length==0) {
+            if (map.size()==0 || userInput.equals("NEW")) {
                 userInput = ui.askInput("Введите новую заметку, BACK - к списку заметок или EXIT - выйти из программы");
                 if (!userInput.equals("BACK")) storage.add(new Task(userInput, new Date(), "Wait"));
             }
             else {
                 userInput = ui.askInput("Выберете номер пункта, создайте новую задачу (NEW) или EXIT - выйти из программы");
-                if (userInput.equals("NEW")) {
-                    userInput = ui.askInput("Введите новую заметку, BACK - к списку заметок или EXIT - выйти из программы");
-                    if (!userInput.equals("BACK")) storage.add(new Task(userInput, new Date(), "Wait"));
-                } else {
-                    int taskIndex = getIndex(userInput, arrTask);
-                    if (taskIndex != -1) do {
-                        userInput = ui.askInput("DEL - удалить, DONE - пометить как выполненное, WAIT - возобновить задачу," +
-                                    " BACK - к списку заметок или EXIT - выйти из программы ");
-                        if (userInput.equals("DONE")) storage.setStatus(map.get(taskIndex).getId(),"Done");
-                        if (userInput.equals("WAIT")) storage.setStatus(map.get(taskIndex).getId(),"Wait");
-                        if (userInput.equals("DEL")) storage.setStatus(map.get(taskIndex).getId(),"ARCH");
-                    } while (!Arrays.asList(new String[] {"DEL", "DONE", "WAIT", "BACK", "EXIT"}).contains(userInput));
-                }
+                int taskIndex = getIndex(userInput, map);
+                if (taskIndex != -1) do {
+                    userInput = ui.askInput("DEL - удалить, DONE - пометить как выполненное, WAIT - возобновить задачу," +
+                                " BACK - к списку заметок или EXIT - выйти из программы ");
+                    if (userInput.equals("DONE")) storage.setStatus(map.get(taskIndex).getId(),"Done");
+                    if (userInput.equals("WAIT")) storage.setStatus(map.get(taskIndex).getId(),"Wait");
+                    if (userInput.equals("DEL")) storage.setStatus(map.get(taskIndex).getId(),"ARCH");
+                } while (!Arrays.asList(new String[] {"DEL", "DONE", "WAIT", "BACK", "EXIT"}).contains(userInput));
             }
         }
     }
 
-    static int getIndex (String userInput, Task[] tasks) {
+    static int getIndex (String userInput, Map<Integer, Task> map) {
         try {
             int taskIndex = Integer.parseInt(userInput);
-            if (ValueRange.of(1, tasks.length).isValidIntValue(taskIndex)) return taskIndex;
+            if (ValueRange.of(1, map.size()).isValidIntValue(taskIndex)) return taskIndex;
             return -1;
         } catch (NumberFormatException e) { return -1; }
     }
