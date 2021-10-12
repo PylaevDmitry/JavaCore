3a3e8a99e4cdadf89186940d0296caf8c8dcf565
Dmitry Pylaev <pylaevdmitry@yandex.ru>
2021-10-12 08:19:21 +0400

    ConsoleUserInterface refactoring, Stepic package

--- src/main/java/stepic/Main.java
+++ src/main/java/stepic/Main.java
@@ -1,13 +1,18 @@
 package stepic;
 
+import java.util.*;
+
 public class Main {
     public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
+        int[] arr = Arrays.stream(scanner.next().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int W = arr[0];
+        int n = arr[1];
+        arr = Arrays.stream(scanner.next().split(" ")).mapToInt(Integer::parseInt).toArray();
 
 
     }
 }
-
-
-
-
-
